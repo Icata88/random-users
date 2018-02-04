7b6869c69bd5c3088133c41ba5098ab62159eb36
Hristo Kozhuharov <ico_kozhuharov88@abv.bg>
2018-02-04 18:28:37 +0200

    random users api
    added mailto reference

--- src/components/Details.js
+++ src/components/Details.js
@@ -19,7 +19,7 @@ let Details = (props) => {
 						<div className='user_details__img col-xs-12'><img src={helperFunctions.getImage(props.user[0])} /></div>			
 						<div className='user_details__row col-xs-12'><p className='col-xs-12'><span>Title: </span>{helperFunctions.getTitle(props.user[0])}</p></div>
 						<div className='user_details__row col-xs-12'><p className='col-xs-12'><span>Name: </span>{helperFunctions.getFullName(props.user[0])}</p></div>			
-						<div className='user_details__row col-xs-12'><p className='col-xs-12'><span>Email: </span>{helperFunctions.getEmail(props.user[0])}</p></div>	
+						<div className='user_details__row col-xs-12'><p className='col-xs-12'><span>Email: </span><a href={`mailto:${helperFunctions.getEmail(props.user[0])}`}>{helperFunctions.getEmail(props.user[0])}</a></p></div>	
 						<div className='user_details__row col-xs-12'><p className='col-xs-12'><span>Phone: </span>{helperFunctions.getPhone(props.user[0])}</p></div>
 						<div className='user_details__row col-xs-12'><p className='col-xs-12'><span>City: </span>{helperFunctions.getCity(props.user[0])}</p></div>
 						<div className='user_details__row col-xs-12'><p className='col-xs-12'><span>Street: </span>{helperFunctions.getStreet(props.user[0])}</p></div>	
